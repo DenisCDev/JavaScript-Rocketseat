b572ea910a317cfe3414bd2d121f03e4a57fb629
DenisCDev <coldgreen04@gmail.com>
2022-07-19 15:26:06 -0300

    Array com livros e mangás

---
+++ 10 - pratica/4script.js
@@ -0,0 +1,38 @@
+// Buscando e contando dados em Arrays
+
+const booksByCategory = [
+    {
+        category: "Programação",
+        books: [
+            {
+                title: "Storytelling com dados: Um guia sobre visualização de dados para profissionais de negócios",
+                author: "Cole Nussbaumer Knaflic",
+            },
+            {
+                title: "Arquitetura limpa: O guia do artesão para estrutura e design de software",
+                author: "Robert C. Martin",
+            },
+            {
+                title: "Algoritmos para viver - A ciência exata das decisões humanas",
+                author: "Brian Christian & Tom Griffiths",
+            },
+        ],
+    },
+    {
+        category: "Manga",
+        books: [
+            {
+                title: "Spy X Family",
+                author: "Tatsuya Endou",
+            },
+            {
+                title: "One Piece",
+                author: "Eiichiro Oda",
+            },
+            {
+                title: "Haikyuu",
+                author: "HARUICHI FURUDATE",
+            },
+        ],
+    },
+];
